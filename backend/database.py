5015271a50f050245930919315f616dda7d3cfdd
Ege Okan Ünaldı <35339130+exeokan@users.noreply.github.com>
2024-12-04 21:30:05 +0300

    Edit backend (#5)
    * edit backend
    
    * revert sql changes

--- backend/database.py
+++ backend/database.py
@@ -1,9 +1,19 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
 
 # MySQL Database URL
-DATABASE_URL = "mysql+pymysql://root:12345678@localhost:3306/phase_2_deneme_1"
+USER = os.getenv("DB_USER")
+PASSWORD = os.getenv("DB_PASSWORD")
+HOST = "localhost"
+PORT = "3306"
+DATABASE = os.getenv("DB_NAME")
+
+DATABASE_URL = f"mysql+pymysql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}"
 
 # Create SQLAlchemy Engine
 engine = create_engine(DATABASE_URL)
