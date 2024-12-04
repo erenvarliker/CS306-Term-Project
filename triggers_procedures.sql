4e47b8c0815fbced43a5e2b43c8d4858902df305
Teoman Arabul <119057447+teo123man@users.noreply.github.com>
2024-12-04 22:47:09 +0300

    Update triggers_procedures.sql

--- triggers_procedures.sql
+++ triggers_procedures.sql
@@ -6,21 +6,27 @@ CREATE TRIGGER decrement_employee_count_on_doctor_delete
 AFTER DELETE ON Doctor
 FOR EACH ROW
 BEGIN
-	UPDATE Department
-	SET num_doctors = num_doctors - 1
-	WHERE department_id = OLD.department_id;
+    UPDATE Department
+    SET num_doctors = num_doctors - 1
+    WHERE department_id = OLD.department_id;
 END$$
 DELIMITER ;
 
 DELIMITER $$
-CREATE PROCEDURE GetDepartmentDetails()
+
+CREATE PROCEDURE UpdateNumDoctors()
 BEGIN
-    SELECT 
-        d.department_id,
-        d.name AS DepartmentName,
-        (SELECT COUNT(*) 
-         FROM Doctor 
-         WHERE Doctor.department_id = d.department_id) AS NumberOfDoctors
-    FROM Department d;
+    -- Update the num_doctors column for each department
+    UPDATE Department d
+    SET num_doctors = (
+        SELECT COUNT(*)
+        FROM Doctor doc
+        WHERE doc.department_id = d.department_id
+    );
+
+    -- Display the updated num_doctors for each department
+    SELECT department_id, name AS DepartmentName, num_doctors
+    FROM Department;
 END$$
+
 DELIMITER ;
