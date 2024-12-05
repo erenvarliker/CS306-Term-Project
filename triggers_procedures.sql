12b55b2801121b2032f3d7ba61a135683ca8053d
Teoman Arabul <119057447+teo123man@users.noreply.github.com>
2024-12-05 23:23:39 +0300

    re-add trigs

--- triggers_procedures.sql
+++ triggers_procedures.sql
@@ -97,3 +97,35 @@ BEGIN
 END$$
 
 DELIMITER ;
+
+--teo a
+ALTER TABLE Department
+ADD COLUMN num_doctors INT DEFAULT 0;
+
+DELIMITER $$
+CREATE TRIGGER decrement_employee_count_on_doctor_delete
+AFTER DELETE ON Doctor
+FOR EACH ROW
+BEGIN
+    UPDATE Department
+    SET num_doctors = num_doctors - 1
+    WHERE department_id = OLD.department_id;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+
+CREATE PROCEDURE UpdateNumDoctors()
+BEGIN
+    UPDATE Department d
+    SET num_doctors = (
+        SELECT COUNT(*)
+        FROM Doctor doc
+        WHERE doc.department_id = d.department_id
+    );
+
+    SELECT department_id, name AS DepartmentName, num_doctors
+    FROM Department;
+END$$
+
+DELIMITER ;
